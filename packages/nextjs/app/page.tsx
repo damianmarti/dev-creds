d884e98e00ee80c5cf28b16f7a1cfa29f42f9207
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-09-02 13:28:07 +0530

    init homepage changes

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -9,8 +9,7 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">DevCreds</span>
+            <span className="block text-2xl mb-2">Build Your Developer Reputation On-Chain</span>
           </h1>
         </div>
         <Attest />
